3856d720491c8a453ed646e823b76cf3be30ca09
Charlotte Fawsitt <charlyfawsitt@gmail.com>
2018-01-22 18:01:19 +0000

    This is the perlin noise sketch, it uses the noise function and a time variable to produce a number close to but not the same as the numebr previous

---
+++ sketch8/sketch8.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(1200, 1200);
+    background(255);
+    walker = new Walker();
+}
+
+function draw() {
+    fill(0, 15);
+    rect(0, 0, width, height);
+
+    walker.render();
+    walker.step();
+}
