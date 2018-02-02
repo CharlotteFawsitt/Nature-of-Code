5d726239c63a74766550e711ffefc1e0e0b26802
Charlotte Fawsitt <charlyfawsitt@gmail.com>
2018-02-02 15:49:40 +0000

    Sketch 16

--- Vectors/Moving_a_ball_2_directions/sketch16.js
+++ Vectors/Moving_a_ball_2_directions/sketch16.js
@@ -1,6 +1,7 @@
-var x = 320;
-var y = 180;
-var xSpeed = 2;
+var x = 100;
+var y = 100;
+var xSpeed = 2.5;
+var ySpeed = 2;
 
 function setup() {
     createCanvas(640, 360);
@@ -11,15 +12,19 @@ function draw() {
 
     //Add the current speed to the position
     x = x + xSpeed;
+    y = y + ySpeed;
 
     //Check to see if the position is off the canvas
     if ((x + 24 > width || x - 24 < 0)) {
         xSpeed = xSpeed * -1;
+    }
+    if ((y + 24 > height || y - 24 < 0)) {
+        ySpeed = ySpeed * -1;
     }
 
     //Display a circle at the x position
     stroke(0);
     strokeWeight(2);
     fill(127);
-    ellipse(x, 180, 48, 48);
+    ellipse(x, y, 48, 48);
 }
